3a35b5e75a14fb4fd8efa707054226ada4de793b
Gabriel Pires <gabrielhpires@gmail.com>
2020-11-02 20:31:53 -0300

    .

--- qlearning.py
+++ qlearning.py
@@ -8,7 +8,7 @@ class Qlearning(object):
         self.lambda_ = lambda_
         self.epsilon = epsilon
         self.n = n
-        self.steps_left = warehouse.max_steps
+        self.steps = 0
         self.actual_state = warehouse.random_valid_position()
         self.moves = ["RIGHT", "LEFT", "UP", "DOWN"]
 
@@ -21,10 +21,10 @@ class Qlearning(object):
             self.q_table[str(i)] = {}
             for j in range(len(self.warehouse.map_[i])):
                 self.q_table[str(i)][str(j)] = {}
-                # for steps_left in range(0, self.steps_left + 1):
-                #     self.q_table[str(i)][str(j)][str(steps_left)] = {}
-                for move in self.moves:
-                    self.q_table[str(i)][str(j)].update({move: 0.0})
+                for steps in range(0, self.warehouse.max_steps + 1):
+                    self.q_table[str(i)][str(j)][str(steps)] = {}
+                    for move in self.moves:
+                        self.q_table[str(i)][str(j)][str(steps)].update({move: 0.0})
 
     def show_q_table(self):
 
@@ -32,12 +32,12 @@ class Qlearning(object):
         q_table = ""
         for x in self.q_table:
             for y in self.q_table[x]:
-                # for steps_left in self.q_table[x][y]:
-                for move in self.q_table[x][y]:
-                    if self.warehouse.map_[int(x)][int(y)] != "*":
-                        q_table_size += 1
-                        q_table += f"({x}, {y}, {0000}), {move},"
-                        q_table += f'{"%0.2f" % self.q_table[x][y][move]}\n'
+                for steps in self.q_table[x][y]:
+                    for move in self.q_table[x][y][steps]:
+                        if self.warehouse.map_[int(x)][int(y)] != "*":
+                            q_table_size += 1
+                            q_table += f"({x}, {y}, {steps}), {move},"
+                            q_table += f'{"%0.2f" % self.q_table[x][y][steps][move]}\n'
         print(q_table)
         print("Q-Table size:", q_table_size)
 
@@ -62,6 +62,8 @@ class Qlearning(object):
         # Se o estado for "." a recompensa é -1
         if self.warehouse.map_[i][j] == ".":
             reward = -1
+            if self.steps >= self.warehouse.max_steps + 1:
+                reward -= 10
         # Se o estado for "#" a recompensa é +1
         elif self.warehouse.map_[i][j] == "#":
             reward = 1
@@ -70,7 +72,7 @@ class Qlearning(object):
             reward = 10
         # Se o estado for "*" a recompensa é -10
         # Se o número de passos restantes é 0 a recompensa é -10
-        elif self.warehouse.map_[i][j] == "*":  # or self.steps_left - 1 <= 0:
+        elif self.warehouse.map_[i][j] == "*":
             reward = -10
 
         return reward
@@ -95,15 +97,18 @@ class Qlearning(object):
         if new_y < 0 or new_y > self.warehouse.map_width - 1:
             return
 
-        steps_left_before = self.steps_left
-        self.steps_left -= 1
+        steps_before = self.steps
 
-        # if self.steps_left <= 0:
+        # # # # # # # # #
+        # self.steps += 1
+        # # # # # # # # #
+
+        # if self.steps >= self.warehouse.max_steps + 1:
         #     return
 
         # Encontra um ponto de localização e reseta o número de passos
-        if self.warehouse.map_[new_x][new_y] == "#":
-            self.steps_left = self.warehouse.max_steps
+        # if self.warehouse.map_[new_x][new_y] == "#":
+        #     self.steps = 0
 
         # if self.warehouse.map_[new_x][new_y] == "*":
         #     new_x = self.actual_state[0]
@@ -114,28 +119,28 @@ class Qlearning(object):
 
         old_x = str(self.actual_state[0])
         old_y = str(self.actual_state[1])
+        if self.steps >= self.warehouse.max_steps:
+            max_new_state = -10
+        else:
+            max_new_state = self.q_table[new_x][new_y][str(self.steps + 1)][
+                max(
+                    self.q_table[new_x][new_y][str(self.steps + 1)],
+                    key=self.q_table[new_x][new_y][str(self.steps + 1)].get,
+                )
+            ]
 
-        max_new_state = self.q_table[new_x][new_y][  # [str(self.steps_left)][
-            max(
-                self.q_table[new_x][new_y],  # [str(self.steps_left)],
-                key=self.q_table[new_x][new_y].get,  # [str(self.steps_left)].get,
-            )
-        ]
-
-        self.q_table[old_x][old_y][direction] += self.alpha * (  # [str(steps_left_before)][
+        self.q_table[old_x][old_y][str(steps_before)][direction] += self.alpha * (
             self.reward(new_x, new_y)
             + self.lambda_ * max_new_state
-            - self.q_table[old_x][old_y][direction]  # [str(steps_left_before)][
+            - self.q_table[old_x][old_y][str(steps_before)][direction]
         )
 
         self.actual_state = (int(new_x), int(new_y))
         new_state = self.warehouse.map_[self.actual_state[0]][self.actual_state[1]]
 
-        if new_state == "*" or new_state == "$":  # or self.steps_left <= 0:
+        if new_state == "*" or new_state == "$" or self.steps >= self.warehouse.max_steps + 1:
             # Fim do episodio
             self.actual_state = self.warehouse.random_valid_position()
-            self.steps_left = self.warehouse.max_steps
-
             self.n -= 1
 
             mean_reward = 0
@@ -145,38 +150,45 @@ class Qlearning(object):
                     position = self.warehouse.map_[int(x)][int(y)]
                     if position != "." and position != "#":
                         continue
-                    for action in self.q_table[x][y]:  # [str(self.steps_left)]:
-                        mean_reward += self.q_table[x][y][action]  # [str(self.steps_left)]
+                    for action in self.q_table[x][y][str(self.steps)]:
+                        mean_reward += self.q_table[x][y][str(self.steps)][action]
                         num_rewards += 1
 
             mean_reward /= num_rewards
             self.mean_reward.append([self.episode, mean_reward])
             self.episode += 1
+            # # # # # # # # #
+            # self.steps = 0
+            # # # # # # # # #
 
     def show_mean_reward(self):
         values = ""
         for episode in self.mean_reward:
             values += str(episode[0]) + " " + str(episode[1]) + "\n"
-        return values
+        print(values)
 
     def learn(self):
-        while self.n:
-            # if self.steps_left <= 0:
-            #     self.steps_left = self.warehouse.max_steps
-            if random.uniform(0, 1) < self.epsilon:
-                # Exploration
-                self.move(random.choice(self.moves))
-            else:
-                # Exploitation
-                x = str(self.actual_state[0])
-                y = str(self.actual_state[1])
-                # from pprint import pp
+        n = self.n
+        for i in range(self.warehouse.max_steps + 1, 0, -1):
+            self.steps = i
+            self.n = n
+            while self.n:
+                # if self.steps_left <= 0:
+                #     self.steps_left = self.warehouse.max_steps
+                if random.uniform(0, 1) < self.epsilon:
+                    # Exploration
+                    self.move(random.choice(self.moves))
+                else:
+                    # Exploitation
+                    x = str(self.actual_state[0])
+                    y = str(self.actual_state[1])
+                    # from pprint import pp
 
-                # pp(self.q_table)
-                # print(self.q_table[x][y][str(self.steps_left)].get)
-                # print(x, y)
-                best_move = max(
-                    self.q_table[x][y],  # [str(self.steps_left)],
-                    key=self.q_table[x][y].get,  # [str(self.steps_left)].get,
-                )
-                self.move(best_move)
+                    # pp(self.q_table)
+                    # print(self.q_table[x][y][str(self.steps_left)].get)
+                    # print(x, y)
+                    best_move = max(
+                        self.q_table[x][y][str(self.steps)],
+                        key=self.q_table[x][y][str(self.steps)].get,
+                    )
+                    self.move(best_move)
